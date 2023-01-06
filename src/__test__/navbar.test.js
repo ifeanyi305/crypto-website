7e2ceea2b8f02ae86b746cdbd6c73b3af6074965
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-01-06 14:22:38 +0100

    fix linter error

--- src/__test__/navbar.test.js
+++ src/__test__/navbar.test.js
@@ -1,13 +1,13 @@
-import Navbar from "../component/Navbar";
 import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
+import Navbar from '../component/Navbar';
 
-it("Renders navbar component", () => {
-  const tree = renderer 
-  .create(
-    <Router>
-      <Navbar />
-    </Router>
-  ).toJSON();
+it('Renders navbar component', () => {
+  const tree = renderer
+    .create(
+      <Router>
+        <Navbar />
+      </Router>,
+    ).toJSON();
   expect(tree).toMatchSnapshot();
-})
+});
